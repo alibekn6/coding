fcedfda33182e4cbe5bb245d2f5763e265492a42
alibekn6 <alibekandanuarbek@gmail.com>
2023-02-04 21:16:29 +0600

    File system DONEgit add -A

--- people.js
+++ people.js
@@ -1,4 +1,4 @@
-const people = ['yoshi', 'ruy', 'chun-li', 'kotoq'];
+const people = ['yoshi', 'ruy', 'chun-li', 'lesssgooooo'];
 const ages = [20, 25, 30, 35];
 const jobs = ['QA', 'FrontEnd', 'BackEnd', 'Mobile']
 
