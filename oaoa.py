6ff8b73290e818a0f0b28db57d0b7890ef65aa3f
alibekN54 <alibekandanuarbek@gmail.com>
2023-02-06 21:04:26 +0600

    python

--- oaoa.py
+++ oaoa.py
@@ -4,7 +4,11 @@ for i in range(1, 11):
     x = int(input()) 
     if x < 0: 
         s += x 
-    if x > mx: 
-        mx = x 
-print(s) 
-print(mx)
+    if x < mx: 
+        mx = x
+
+if s == 0:
+    print("NO")
+else:
+    print(s)
+    print(mx)
